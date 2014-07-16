a8155dba70c702f5a5a9f1d9f336c24b67df1d10
t0k0r010 <osawa_fi_whi@yahoo.co.jp>
2014-07-16 14:28:30 +0900

    first commit

---
+++ Tennis/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace Tennis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.serveResult = new System.Windows.Forms.GroupBox();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dougaPlayerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.開くToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Court = new System.Windows.Forms.Panel();
+            this.cursorPosition = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.moviePos = new System.Windows.Forms.Label();
+            this.cursorText = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.serveResult.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.serveResult);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Location = new System.Drawing.Point(803, 34);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(339, 703);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // serveResult
+            // 
+            this.serveResult.Controls.Add(this.radioButton5);
+            this.serveResult.Controls.Add(this.radioButton4);
+            this.serveResult.Controls.Add(this.radioButton3);
+            this.serveResult.Location = new System.Drawing.Point(20, 124);
+            this.serveResult.Name = "serveResult";
+            this.serveResult.Size = new System.Drawing.Size(313, 62);
+            this.serveResult.TabIndex = 1;
+            this.serveResult.TabStop = false;
+            this.serveResult.Text = "サーブ球種";
+            this.serveResult.Enter += new System.EventHandler(this.groupBox3_Enter);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(234, 37);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(71, 19);
+            this.radioButton5.TabIndex = 2;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "スライス";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(118, 37);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(61, 19);
+            this.radioButton4.TabIndex = 1;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "スピン";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 37);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(68, 19);
+            this.radioButton3.TabIndex = 0;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "フラット";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.Location = new System.Drawing.Point(20, 42);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(292, 48);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "サーブ種別";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(160, 23);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(51, 19);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "2nd";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(21, 23);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(48, 19);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "1st";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dougaPlayerToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1149, 31);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dougaPlayerToolStripMenuItem
+            // 
+            this.dougaPlayerToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.開くToolStripMenuItem});
+            this.dougaPlayerToolStripMenuItem.Name = "dougaPlayerToolStripMenuItem";
+            this.dougaPlayerToolStripMenuItem.Size = new System.Drawing.Size(127, 27);
+            this.dougaPlayerToolStripMenuItem.Text = "動画プレイヤー";
+            // 
+            // 開くToolStripMenuItem
+            // 
+            this.開くToolStripMenuItem.Name = "開くToolStripMenuItem";
+            this.開くToolStripMenuItem.Size = new System.Drawing.Size(110, 28);
+            this.開くToolStripMenuItem.Text = "開く";
+            this.開くToolStripMenuItem.Click += new System.EventHandler(this.OpenToolStripMenuItem_Click);
+            // 
+            // Court
+            // 
+            this.Court.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Court.Location = new System.Drawing.Point(335, 34);
+            this.Court.Name = "Court";
+            this.Court.Size = new System.Drawing.Size(462, 673);
+            this.Court.TabIndex = 3;
+            // 
+            // cursorPosition
+            // 
+            this.cursorPosition.AutoSize = true;
+            this.cursorPosition.Location = new System.Drawing.Point(46, 86);
+            this.cursorPosition.Name = "cursorPosition";
+            this.cursorPosition.Size = new System.Drawing.Size(83, 15);
+            this.cursorPosition.TabIndex = 4;
+            this.cursorPosition.Text = "カーソル位置";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // moviePos
+            // 
+            this.moviePos.AutoSize = true;
+            this.moviePos.Location = new System.Drawing.Point(49, 145);
+            this.moviePos.Name = "moviePos";
+            this.moviePos.Size = new System.Drawing.Size(43, 15);
+            this.moviePos.TabIndex = 5;
+            this.moviePos.Text = "label1";
+            // 
+            // cursorText
+            // 
+            this.cursorText.Location = new System.Drawing.Point(49, 194);
+            this.cursorText.Name = "cursorText";
+            this.cursorText.Size = new System.Drawing.Size(100, 22);
+            this.cursorText.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1149, 743);
+            this.Controls.Add(this.cursorText);
+            this.Controls.Add(this.moviePos);
+            this.Controls.Add(this.cursorPosition);
+            this.Controls.Add(this.Court);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.serveResult.ResumeLayout(false);
+            this.serveResult.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dougaPlayerToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox serveResult;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Panel Court;
+        private System.Windows.Forms.Label cursorPosition;
+        private System.Windows.Forms.ToolStripMenuItem 開くToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label moviePos;
+        private System.Windows.Forms.TextBox cursorText;
+    }
+}
+
---
+++ Tennis/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Collections;
+using Microsoft.Office.Interop;
+namespace Tennis
+{
+    public partial class Form1 : Form
+    {
+        Point cursor = new Point();
+        MediaPlayerForm mediaPlayer = null;
+        double mediaPos = 0;
+
+        ArrayList BoundPositions = new ArrayList();
+
+
+        public Form1()
+        {
+            //初期設定を書く
+            InitializeComponent();
+
+            this.Court.Paint += new System.Windows.Forms.PaintEventHandler(this.DrawCourt);
+            this.Court.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ClickCourt);
+
+            //Observeという関数を別に実行する.
+            Thread thread = new Thread(Observe);
+            thread.Start();
+        }
+
+        delegate void SetMedia();
+
+        private void Observe()
+        {
+            while (!this.IsDisposed)
+            {
+                if (mediaPlayer == null || mediaPlayer.axWindowsMediaPlayer1.IsDisposed)
+                    continue;
+
+                mediaPos = mediaPlayer.axWindowsMediaPlayer1.Ctlcontrols.currentPosition;
+                this.Invoke( new SetMedia(SetMediaPos));
+            }
+        }
+
+        private void SetMediaPos()
+        {
+            moviePos.Text = mediaPos.ToString();
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        void Output()
+        {
+            string fileName = "C:/Users/Taka/Documents/Visual Studio 2012/Projects/Tennis/Tennis/bin/Debug/test.xlsx";
+            Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();//neExel.Application();
+            xlApp.Visible = false;
+
+            Microsoft.Office.Interop.Excel.Workbook wb = xlApp.Workbooks.Open(Filename: fileName);
+            try
+            {
+                ((Microsoft.Office.Interop.Excel.Worksheet)wb.Sheets[1]).Select();
+            }
+            catch (Exception ex)
+            {
+                wb.Close(false);
+                xlApp.Quit();
+                MessageBox.Show("開けませんでした");
+                return;
+            }
+
+            Microsoft.Office.Interop.Excel.Range CellRange;
+
+            for(int i=0; i<BoundPositions.Count; i++)
+            {
+                CellRange = xlApp.Cells[i+1, 1] as Microsoft.Office.Interop.Excel.Range;
+                CellRange.Value2 = ((Point)BoundPositions[i]).X + "," + ((Point)BoundPositions[i]).Y;
+            }
+
+            wb.Close();
+            xlApp.Quit();
+            MessageBox.Show("出力完了");
+        }
+        private void ClickCourt(object sender, MouseEventArgs e)
+        {
+            //左クリックで新しくバウンド位置を追加
+            if (e.Button == MouseButtons.Left)
+            {
+                cursor = new Point(e.X, e.Y);
+                cursorPosition.Text = "(" + e.X + "," + e.Y + ")";
+
+                cursorText.Text = e.X + "," + e.Y;
+
+                BoundPositions.Add(cursor);
+            }
+                //右クリックでバウンドの軌跡を消去
+            else if (e.Button == MouseButtons.Right)
+            {
+                Output();
+                BoundPositions.Clear();
+            }
+            Court.Invalidate(); //再描画しろっていう命令
+        }
+
+        //コートを描画する
+        private void DrawCourt(object sender, PaintEventArgs e)
+        {
+            Panel panel = (Panel)sender;
+            Graphics g = e.Graphics;
+            Pen pen = new Pen(Color.Black);
+         //   g.DrawRectangle(pen, 0, 0, panel.Width/2, panel.Height/2);
+
+            int courtWidth = panel.Width /2;
+            int courtHeight = (int)(courtWidth / 10.97f * 23.77f);
+
+            //実際の線とプログラム上の線との単位変換
+            float scale = 10.97f / courtWidth; //[m/pixel]
+            // 100 [pixel] * scale => xxx[m] に変換
+            // 100 [m] / scale     => yyy[pixel] に変換
+
+            //コートの外枠を描画
+            Point courtUpLeft = new Point( panel.Width / 2 - courtWidth / 2, panel.Height / 2 - courtHeight / 2);
+            g.DrawRectangle(pen, courtUpLeft.X, courtUpLeft.Y, courtWidth, courtHeight);
+
+            //ネットの描画
+            g.DrawLine(pen, new Point( panel.Width / 2 - courtWidth / 2,
+                panel.Height / 2), new Point(panel.Width / 2 + courtWidth / 2, panel.Height / 2));
+
+            //シングルラインの描画
+            int singleBaseLineWidth = (int)(courtWidth * 8.23f / 10.97f);
+            int singleBaleLineX = courtUpLeft.X + (courtWidth - singleBaseLineWidth) / 2;
+            g.DrawLine(pen, new Point(singleBaleLineX, courtUpLeft.Y),
+                new Point(singleBaleLineX, courtUpLeft.Y + courtHeight));
+            g.DrawLine(pen, new Point(panel.Width / 2 + singleBaseLineWidth / 2, courtUpLeft.Y)
+                , new Point(panel.Width / 2 + singleBaseLineWidth / 2, courtUpLeft.Y+courtHeight));
+
+            //サービスラインの描画
+            int serviceLineHeight = (int)(courtHeight * 12.8 / 23.77);
+            g.DrawLine(pen, new Point(singleBaleLineX, panel.Height / 2 - serviceLineHeight / 2),
+                new Point(singleBaleLineX + singleBaseLineWidth, panel.Height / 2 - serviceLineHeight / 2));
+            g.DrawLine(pen, new Point(singleBaleLineX, panel.Height / 2 + serviceLineHeight / 2),
+                new Point(singleBaleLineX + singleBaseLineWidth, panel.Height / 2 + serviceLineHeight / 2));
+
+            //中心の縦ラインを描画
+            g.DrawLine(pen, new Point(panel.Width / 2, panel.Height / 2 - serviceLineHeight / 2), new Point(panel.Width / 2, panel.Height / 2 + serviceLineHeight / 2));
+
+            //補助線を描画
+
+           // g.DrawEllipse(pen, cursor.X-5 , cursor.Y-5, 10, 10);
+
+            //クリックした点を円で描画
+            Point center = new Point(panel.Width / 2, panel.Height / 2);
+            foreach (Point pos in BoundPositions)
+            {
+                g.DrawEllipse(pen, pos.X - 5, pos.Y - 5, 10, 10);
+                Point p = new Point(pos.X - center.X, pos.Y-center.Y) ;
+                float x = (float)Math.Round(scale * p.X, 2, MidpointRounding.AwayFromZero);
+                float y = (float)Math.Round(scale * p.Y, 2, MidpointRounding.AwayFromZero);
+                string text = x + ", " + y;
+                float width = 10 * text.Length;
+
+                RectangleF rect = new RectangleF(pos.X - width / 2, pos.Y + 5, width, 30);
+
+                StringFormat format = new StringFormat();
+                format.Alignment = StringAlignment.Center;
+                format.LineAlignment = StringAlignment.Center;
+                g.DrawString(text, new Font("Arial", 8), Brushes.Black, rect, format);
+            }
+            /*
+            if (cursor.X < courtUpLeft.X)
+            {
+                cursorPosition.Text = "Out";
+            } else {
+                cursorPosition.Text = "In";
+            }*/
+        }
+
+        private void OpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //ファイルダイアログを開く
+            DialogResult res = openFileDialog1.ShowDialog();
+
+            //開くボタンを押したとき
+            if (res == System.Windows.Forms.DialogResult.OK)
+            {
+                //MediaPlayerのウィンドウを新しく開く
+                mediaPlayer = new MediaPlayerForm();
+                mediaPlayer.axWindowsMediaPlayer1.URL = openFileDialog1.FileName;
+                mediaPlayer.Show();
+            }
+        }
+    }
+}
