a1c3109d505fce29719ea27aadae0f82e3bbe887
Ren Mizutani <nightfall-immortal-longview@live.jp>
2014-10-04 18:02:59 +0900

    コートの描画とエクセル書き出しを別ファイル化した

---
+++ Tennis/Court.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+//using System.Math;
+namespace Tennis
+{
+    class Court
+    {
+        const float Width_m = 10.97f;    //現実のコートのサイズ[m]
+        const float Height_m = 23.77f;
+        const float SingleCourtWidth_m = 8.23f;   //シングルコートの幅
+        const float ServiceLineHeight_m = 6.4f;  //ネットからサービスラインまでの長さ
+
+        int Width_p { get; set; }       //画面上のコートのサイズ [pixel]
+        int Height_p { get; set; }
+        Point Center_p { get; set; }    //コートの中心位置 [pixel]
+
+        List<Point> BoundPositions_p = new List<Point>();
+
+        Panel Panel;
+        public Court(Panel panel)
+        {
+            this.Panel = panel;
+            panel.Paint += new System.Windows.Forms.PaintEventHandler(Draw);
+            panel.MouseClick += MouseClick;
+
+            SetCourtSize(panel);
+        }
+
+        //コートをクリック => バウンド位置を設定
+        void MouseClick(object sender, MouseEventArgs e)
+        {
+            if (!Form1.IsStarted)
+                return;
+
+            if (e.Button == MouseButtons.Left)
+            {
+                BoundPositions_p.Add(new Point(e.X, e.Y));
+                PointF point_m = ToRealUnit(e.X, e.Y);
+                ExcelWriter.Instance.SetBoundPosition(MediaPlayerForm.Instance.GetCurrentTimeText(), point_m.X, point_m.Y);
+            }
+            else
+            {
+                BoundPositions_p.Clear();
+            }
+
+            Panel.Invalidate(); //再描画命令
+        }
+
+        void Draw(Object panel, PaintEventArgs e)
+        {
+            SetCourtSize((Panel)panel);    //サイズ変わった時の為に毎回セットする.
+            DrawCourt(e.Graphics);
+            DrawBoundMarks(e.Graphics);
+        }
+
+        //コートのサイズを再計算
+        void SetCourtSize(Panel panel)
+        {
+            //パネルの半分の大きさにする
+            Width_p  = panel.Width / 2;                                
+            Height_p = (int)Math.Round(Width_p / Width_m * Height_m);
+
+            //中心を原点とする.
+            Center_p = new Point(panel.Width / 2, panel.Height / 2);
+        }
+
+        // [m] -> [pixel] 変換
+        public int MeterToPixel(float meter)
+        {
+            //整数にするので四捨五入
+            return (int)Math.Round(meter * Width_p / Width_m);
+        }
+
+        // [pixel] -> [m] 変換
+        public float PixelToMeter(int pixel)
+        {
+            return pixel * Width_m / Width_p;
+        }
+
+        //コートの中心を原点とした,実世界の[m]単位に変換
+        public PointF ToRealUnit(Point p)
+        {
+            float x = (float)Math.Round( PixelToMeter(p.X - Center_p.X), 2, MidpointRounding.AwayFromZero);
+            float y = (float)Math.Round( PixelToMeter(p.Y - Center_p.Y), 2, MidpointRounding.AwayFromZero);
+
+            return new PointF(x, y);
+        }
+        //コートの中心を原点とした,実世界の[m]単位に変換
+        public PointF ToRealUnit(int p_x, int p_y)
+        {
+            float x = (float)Math.Round(PixelToMeter(p_x - Center_p.X), 2, MidpointRounding.AwayFromZero);
+            float y = (float)Math.Round(PixelToMeter(p_y - Center_p.Y), 2, MidpointRounding.AwayFromZero);
+
+            return new PointF(x, y);
+        }
+        //コートの描画
+        void DrawCourt(Graphics g)
+        {
+            //線の色やサイズを決める.
+            Pen pen = new Pen(Color.Black);
+
+            //コートの左上座標を計算
+            Point upperLeft = new Point(Center_p.X - Width_p / 2, Center_p.Y - Height_p / 2);
+
+            //外枠を描画
+            g.DrawRectangle(pen, upperLeft.X, upperLeft.Y, Width_p, Height_p);
+
+            //シングルラインの描画
+            int singleCourtWidth_p = MeterToPixel(SingleCourtWidth_m);
+            g.DrawLine(pen,                                                 //左のライン
+                Center_p.X - singleCourtWidth_p / 2, upperLeft.Y,
+                Center_p.X - singleCourtWidth_p / 2, upperLeft.Y + Height_p);
+            g.DrawLine(pen,                                                 //右のライン
+                Center_p.X + singleCourtWidth_p / 2, upperLeft.Y,
+                Center_p.X + singleCourtWidth_p / 2, upperLeft.Y + Height_p);
+
+            //サービスラインの描画
+            int serviceLineHeight_p = MeterToPixel(ServiceLineHeight_m);
+            g.DrawLine(pen,
+                Center_p.X - singleCourtWidth_p / 2, Center_p.Y - serviceLineHeight_p,    //上のライン
+                Center_p.X + singleCourtWidth_p / 2, Center_p.Y - serviceLineHeight_p);
+            g.DrawLine(pen,
+                Center_p.X - singleCourtWidth_p / 2, Center_p.Y + serviceLineHeight_p,    //下のライン
+                Center_p.X + singleCourtWidth_p / 2, Center_p.Y + serviceLineHeight_p);
+
+            //中心のラインの描画
+            g.DrawLine(pen,
+               Center_p.X, Center_p.Y - serviceLineHeight_p,
+               Center_p.X, Center_p.Y + serviceLineHeight_p);
+        }
+
+        //バウンド跡を描画
+        void DrawBoundMarks(Graphics g)
+        {
+            Pen pen = new Pen(Color.Red);
+            int markerSize = Width_p / 10;
+            foreach (Point p in BoundPositions_p)
+            {
+                g.DrawEllipse(pen, p.X - markerSize / 2, p.Y - markerSize / 2, markerSize, markerSize);
+            }
+        }
+        
+
+        /*
+private void ClickCourt(object sender, MouseEventArgs e)
+{
+    //左クリックで新しくバウンド位置を追加
+    if (e.Button == MouseButtons.Left)
+    {
+        cursor = new Point(e.X, e.Y);
+        cursorPosition.Text = "(" + e.X + "," + e.Y + ")";
+
+        cursorText.Text = e.X + "," + e.Y;
+
+        BoundPositions.Add(cursor);
+    }
+        //右クリックでバウンドの軌跡を消去
+    else if (e.Button == MouseButtons.Right)
+    {
+        Output();
+        BoundPositions.Clear();
+    }
+    CourtPannel.Invalidate(); //再描画しろっていう命令
+}*/
+    }
+}
---
+++ Tennis/ExcelWriter.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft;
+using Microsoft.Office;
+using Microsoft.Office.Interop;
+
+//エクセルファイルへの書き出しを行うクラス
+//どんなクラスからもアクセスできるが,このクラスは誰も保持しない.
+namespace Tennis
+{
+    using Excel = Microsoft.Office.Interop.Excel;
+    class ExcelWriter
+    {
+        public static ExcelWriter Instance {get; private set;}
+
+        //ファイルを開く
+        public static void Open()
+        {
+            //すでにファイルを開いているときは,新しく開きなおすか確認する.
+            if (Instance != null)
+            {
+                if (MessageBox.Show("今あるファイルを閉じて別のファイルを開きますか?", "確認", 
+                    MessageBoxButtons.OKCancel) == DialogResult.Cancel)
+                    return;
+
+                Instance.Close();
+            }
+
+            //ファイルを開く
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "エクセルファイルを開く";
+            //開くボタンを押したとき
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string fileName = dialog.FileName;
+
+                try
+                {
+                    Instance = new ExcelWriter(fileName);
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("開けませんでした", ex.Message);
+                    Instance = null;
+                }
+            }
+        }
+
+        Microsoft.Office.Interop.Excel.Application excelApp;
+        Microsoft.Office.Interop.Excel.Workbook wb;
+
+        uint currentRow = 5;   //現在の行番号
+
+        ExcelWriter(string fileName)
+        {
+            excelApp = new Excel.Application();
+            excelApp.Visible = true;
+            wb = excelApp.Workbooks.Open(Filename: fileName);
+            try
+            {
+                ((Excel.Worksheet)wb.Sheets[1]).Select();
+                MakeTemplate();
+            }
+            catch (Exception ex)
+            {
+                wb.Close(false);
+                excelApp.Quit();
+                throw new Exception();
+            }
+        }
+
+        void Close()
+        {
+            if (excelApp != null)
+            {
+                excelApp.Quit();
+            }
+        }
+
+        //バウンドした座標を書き込む. 
+        public void SetBoundPosition(string time, float x, float y)
+        {
+            Excel.Range range = excelApp.get_Range("AB" + currentRow, "AC"+currentRow);
+            range.get_Range("A1").Value2 = x;
+            range.get_Range("B1").Value2 = y;
+
+            excelApp.get_Range("B" + currentRow).Value2 = time;
+            currentRow++;
+        }
+
+        //列のテンプレートを作成
+        // title  : 黄色の文字で書かれるその項目のタイトル.
+        // left   : その項目の左端の列番号( "A" や "D" と指定する)
+        // right  : 右端の列番号
+        // return : 指定した範囲のセルが戻る "A", "D" と指定すると "A1" から"D4"が返る
+        Excel.Range MakeCol(string title, string left, string right)
+        {
+            Excel.XlBorderWeight lineWeight = Excel.XlBorderWeight.xlMedium;
+
+            Excel.Range r = excelApp.get_Range(left + "1", right + "4");
+            r.get_Range("A1").Value2 = title;
+            r.get_Range("A1").Interior.Color = 0x44FFFF;
+            r.Borders.get_Item(Excel.XlBordersIndex.xlEdgeRight).Weight = lineWeight;
+            r.Borders.get_Item(Excel.XlBordersIndex.xlEdgeBottom).Weight = lineWeight;
+
+            //とりあえず,300行分線を引いておく
+            Excel.Range r2 = excelApp.get_Range(left + "1", right + "300");
+
+            r2.Borders.get_Item(Excel.XlBordersIndex.xlEdgeRight).Weight = lineWeight;
+
+            return r;
+        }
+
+        //テンプレートの生成
+        void MakeTemplate()
+        {
+            //時間の列のテンプレート
+            Excel.Range time = MakeCol("時間", "A", "B");
+            time.get_Range("A2").Value2 = "再生時間";
+            time.get_Range("B2").Value2 = "到達時間";
+
+            //カウントの列のテンプレート
+            Excel.Range count = MakeCol("カウント", "C", "E");
+            count.get_Range("A2").Value2 = "セット";
+            count.get_Range("B2").Value2 = "ゲーム";
+            count.get_Range("C2").Value2 = "ポイント";
+
+            //番号の列
+            Excel.Range number = MakeCol("番号", "F", "G");
+            number.get_Range("A2").Value2 = "ラリー";
+            number.get_Range("B2").Value2 = "ショット";
+
+            //種別の列
+            Excel.Range kind = MakeCol("種別", "H", "K");
+            kind.get_Range("A2").Value2 = "サーブ";
+            kind.get_Range("B2").Value2 = "リターン";
+            kind.get_Range("C2").Value2 = "ラリー";
+            kind.get_Range("D2").Value2 = "endショット";
+
+            //サーブの列
+            Excel.Range serve = MakeCol("サーブ", "L", "AA");
+            /*
+             ここは自分で書いてみてください.
+             * 小さいラインの書き方は今度教えます.
+             */
+
+
+            //「座標」項目テンプレートの作成
+            Excel.Range coordinate = MakeCol("座標", "AB", "AJ");//exelApp.get_Range("AB1", "AJ4");
+            coordinate.get_Range("A2").Value2 = "バウンド";
+            coordinate.get_Range("A3").Value2 = "x";
+            coordinate.get_Range("B3").Value2 = "y";
+        }
+
+    }
+}
